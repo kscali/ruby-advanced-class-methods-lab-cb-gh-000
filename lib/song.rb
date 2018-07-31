4a8d81cf2bab0ab6e4ed73e0b4896baa674fff6e
Koy Saeteurn <kosatun@aol.com>
2018-07-31 19:45:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,9 @@ class Song
   end
 
   def self.create 
-    song = self.new
-    song.name = name 
-    @@all << song 
+    song = Song.new
+    song.save 
+    song
   end   
   
   def self.new_by_name(name)
